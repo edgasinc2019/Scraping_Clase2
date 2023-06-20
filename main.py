9db20b20ea9e30aef7f8deabb98656b74d384675
edgasinc2019 <56145238+edgasinc2019@users.noreply.github.com>
2023-06-20 10:45:00 -0500

    Primer cambio

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from telegram_bot import TelegramBot
+from mongodb import MongoDB
+chrome_driver = webdriver.Chrome()
+
+bot = TelegramBot()
+db = MongoDB()
+
+
+chrome_driver.get("https://www.wikipedia.org/")
+print(chrome_driver.title)
+
+topics = [
+    "Alhambra",
+    "Generalife",
+    "gonzalez suarez"
+]
+
+for t in topics:
+    search_box = chrome_driver.find_element(By.ID, "searchInput")
+    search_box.send_keys(t)
+    search_button = chrome_driver.find_element(By.CSS_SELECTOR, '#search-form > fieldset > button > i')
+    search_button.click()
+
+    content = chrome_driver.find_element(By.ID, "mw-content-text")
+    print(content.text)
+    text = content.text
+    text = text[:4000]
+    bot.send_tg_message(text)
+    db.insert_wikipedia_text(title=t, text=text)
+    time.sleep(2)
+    chrome_driver.get("https://www.wikipedia.org/")
+
+
+print("fin")
+chrome_driver.close()
